f88fe6b3049295cf70a57ebe1747aa6c2c8733ff
zq <2390650467@qq.com>
2022-11-23 20:47:29 +0800

    second commit

--- src/main/java/com.zq.study01/GitTest.java
+++ src/main/java/com.zq.study01/GitTest.java
@@ -10,5 +10,6 @@ package com.zq.study01;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git1");
     }
 }
