55ad00caef54d837aa2a0b2a0fd275b0a9ccae1c
zq <2390650467@qq.com>
2022-11-23 20:31:44 +0800

    first commit

---
+++ src/main/java/com.zq.study01/GitTest.java
@@ -0,0 +1,14 @@
+package com.zq.study01;
+
+/**
+ * @BelongsProject: git-test01
+ * @BelongsPackage: com.zq.study01
+ * @Author: zhouzhou
+ * @CreateTime: 2022-11-23  20:27
+ * @Version: 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
