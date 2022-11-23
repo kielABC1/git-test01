8d450d24ff296df16c558ceeba2632293f49a7ba
zq <2390650467@qq.com>
2022-11-23 21:02:46 +0800

    hot-fix commit

--- src/main/java/com.zq.study01/GitTest.java
+++ src/main/java/com.zq.study01/GitTest.java
@@ -10,5 +10,7 @@ package com.zq.study01;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
