d899e45465fd115af4e101537eb20ce790e2e205
kielABC1 <113677151+kielABC1@users.noreply.github.com>
2022-11-24 17:48:31 +0800

    pull commit

--- src/main/java/com.zq.study01/GitTest.java
+++ src/main/java/com.zq.study01/GitTest.java
@@ -9,9 +9,10 @@ package com.zq.study01;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git0");
         System.out.println("hello git1");
         System.out.println("hello git2");
         System.out.println("push git3");
+        System.out.println("pull git4"); 
     }
 }
